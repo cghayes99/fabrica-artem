e60f2f1ed9f6061d48fb4f1a4bcbe91df7148b08
Chris <chris@brogard.io>
2018-12-12 21:03:52 -0600

    htdocs more

--- working/htdocs/static/fabrica-artem.js
+++ working/htdocs/static/fabrica-artem.js
@@ -1,14 +1,15 @@
+ACTION_UPLOAD_URL_DEV = "http://datalogi.brogard:5050/upload";
+ACTION_UPLOAD_URL_PRD = "http://datalogi.brogard:5050/upload";
+
 $(document).ready(function() {
     $("#artem--upload-modal").click(function() {
         $.FileDialog({
-            title: "Select image to upload",
-            drag_message: "Drop image file your here",
+            title: "Select image(s) to upload",
+            drag_message: "Drop image file(s) your here",
             dropheight: 250,
             cancel_button: "Cancel",
             multiple: true
         }).on("files.bs.filedialog", function(ev) {
-            console.log("### modal open; event fired");
-            var text = "";
             var files = ev.files;
 
             files.forEach(function(f) {
@@ -19,15 +20,19 @@ $(document).ready(function() {
                 $.ajax({
                     type: "POST",
                     enctype: "multipart/form-data",
-                    url: "http://datalogi.brogard:5050/upload/alpha",
+                    url: ACTION_UPLOAD_URL_DEV,
                     data: formData,
                     processData: false,
                     contentType: false,
                     cache: false,
                     timeout: 600000,
                     success: function(data) {
-                        console.log("ajax SUCCESS : ", data);
-                        text += f.name + "<br/>";
+                        console.log("data: "+data);
+                        if (data.status === 500) {
+                            console.log("Message : ", data.message);
+                        } else {
+                            console.log("ajax success");
+                        }
                     },
                     error: function(e) {
                         console.log("ajax ERROR : ", e);
